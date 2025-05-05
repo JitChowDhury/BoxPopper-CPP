61c16286ee0a2a7032086f47be5fb40e01eafb88
JitChowDhury <jitchowdhury8717@gmail.com>
2025-05-05 20:26:35 +0530

    Create First Window

--- Box Popper/main.cpp
+++ Box Popper/main.cpp
@@ -1,24 +1,41 @@
 #include <SFML/Graphics.hpp>
 
+
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    //Window
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Box-Popper", sf::Style::Titlebar | sf::Style::Close);
+    sf::Event ev;
 
-    while (window.isOpen())
+//Game Loop
+
+    while (window.isOpen())//check if open
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+        //event polling
+        while (window.pollEvent(ev)) // whenever the window gets an event its gonna save it to ev
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            switch (ev.type)
+            {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    if (ev.key.code == sf::Keyboard::Escape)
+                     window.close();
+                     break;
+            }
+
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+        //update
 
+        //Render
+        window.clear(sf::Color::Red);//clear old frame
+
+        //Draw your game
+
+        window.display();//tell app that window is done drawing
+    }
     return 0;
 }
